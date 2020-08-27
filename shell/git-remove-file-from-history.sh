7fe48ac99263efb9d78b4a5753d0e07be2754b3c
rac@INTRACTIV.LOCAL <racevedo@intractiv.com>
2020-08-27 13:10:38 +0200

    renaming

--- shell/git-remove-file-from-history.sh
+++ shell/git-remove-file-from-history.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 file_relative_git_path=$1
 if [ -z "$file_relative_git_path" ];
     then
@@ -8,3 +10,6 @@ fi
 git filter-branch --force --index-filter \
   "git rm --cached --ignore-unmatch $file_relative_git_path" \
   --prune-empty --tag-name-filter cat -- --all
+
+echo "This script was guided by https://docs.github.com/en/github/authenticating-to-github/removing-sensitive-data-from-a-repository"
+echo "Now you can run 'git push origin --force --all' and 'git push origin --force --tags'"
