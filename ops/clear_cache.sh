4699a928c4010c9db98b17e88e5d73983aee0597
rac@INTRACTIV.LOCAL <racevedo@intractiv.com>
2020-10-22 15:07:50 +0200

    adding ops script to handle slow servers

---
+++ ops/clear_cache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Clear PageCache, dentries and inodes, then, clear swap
+# Note, we are using "echo 3", but it is not recommended in production instead use "echo 1"
+echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'
+
