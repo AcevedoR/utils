04dc16da17f2491785830e12c24c4ef5689a2c2b
rac@INTRACTIV.LOCAL <racevedo@intractiv.com>
2021-02-10 10:42:19 +0100

    git-remove-file-from-history update

--- shell/git-remove-file-from-history.sh
+++ shell/git-remove-file-from-history.sh
@@ -5,7 +5,7 @@ if [ -z "$file_relative_git_path" ];
     then
         echo "Usage : sh $0 <file relative git path>"
         exit 1
-fi
+fi++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 git filter-branch --force --index-filter \
   "git rm --cached --ignore-unmatch $file_relative_git_path" \
